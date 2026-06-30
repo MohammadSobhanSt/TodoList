b9fdfbd44f677a9b88d5b5dd359d95a8165f63e9
MohammdSobhanSt <habibdokhtmohammadsobhan@gmail.com>
2026-07-01 01:05:47 +0330

    Created very first files of frontend

---
+++ todo-frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Welcome from "./components/Welcome.vue";
+import Navigation from "./components/Navigation.vue";
+</script>
+
+<template>
+    <Navigation />
+    <br>
+    <Welcome />
+</template>
+
+<style scoped>
+</style>
---
+++ todo-frontend/src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card" id="welcomeCard">
+        <div class="card-body">
+            <h4 class="text-lightblue">Welcome to our website :)</h4>
+            <br>
+            <p class="text-lightgreen">This is my first app with vue :)</p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .text-lightblue {
+        color: lightblue;
+    }
+    .text-lightgreen {
+        color: lightgreen;
+    }
+    #welcomeCard {
+        width: 50rem;
+    }
+</style>
